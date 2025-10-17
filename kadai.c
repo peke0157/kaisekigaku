55a33519295e5a2f8d7c9b42e4884808b2907d73
hiro <hiroto117@Hirotovivobook.localdomain>
2025-10-17 11:03:26 +0900

    Add a command

--- kadai.c
+++ kadai.c
@@ -1,4 +1,19 @@
 #include<ctype.h>
 #include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    // ファイルポインタの宣言
+    FILE *inputFile;
+    FILE *outputFile;
+
+    char inputFilename = "Input.txt";
+    char outputFilename = "output.txt";
+
+    int ch; // 読み込んだ文字を格納
+
+    
+    
+}
 
 
