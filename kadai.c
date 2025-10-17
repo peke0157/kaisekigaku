e5b8454e20e6da031b7acd1ca4e52aa4ad6afedd
hiro <hiroto117@Hirotovivobook.localdomain>
2025-10-17 13:58:16 +0900

    Add a command

--- kadai.c
+++ kadai.c
@@ -1,8 +1,9 @@
-#include<ctype.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
+int main(void)
+{
     // ファイルポインタの宣言
     FILE *inputFile;
     FILE *outputFile;
@@ -12,8 +13,28 @@ int main(void){
 
     int ch; // 読み込んだ文字を格納
 
+    // 入力ファイルを開く
+    inputFile = fopen(inputFilename, "r");
+    for (inputFile == NULL)
+    {
+        printf("エラー：入力ファイル '%s' を開けません。\n", inputFilename);
+        exit(1);
+    }
+    
+    // 出力ファイルを開く
+    outputFile = fopen(outputFilename, "r");
+    for (outputFile == NULL)
+    {
+        printf("エラー：出力ファイル '%s' を開けません。\n", outputFilename);
+        fclose(inputFile);      //　開いている入力ファイルを閉じる
+        exit(1);
+    }
+
+    // メインループ処理
+    while (condition)
+    {
+        /* code */
+    }
     
     
 }
-
-
