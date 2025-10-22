a10e1dd1f6dbb020b72142b4af29361e6901318f
hiro <hiroto117@Hirotovivobook.localdomain>
2025-10-22 20:50:43 +0900

    test program

--- kadai3.c
+++ kadai3.c
@@ -2,29 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_UNIGUE_BIGRAMS 5000
 // 2組と出現頻度を保存する構造体
 typedef struct 
 {
     char bigram[3];     // ex) 'he' + '0\' で3バイト
     int count;
 }BigramFreq;
+BigramFreq list[MAX_UNIGUE_BIGRAMS];
+int unique_bigram_count = 0;
 
 // 3組と出現頻度を保存する構造体
 typedef struct{
     char ngram[4];      // 'the' + '\0' で4バイト
     int count;
 }Ngram;
+Ngram list[MAX_UNIGUE_BIGRAMS];
+int unique_ngram_count = 0;
 
 int main(void)
 {
+   
+
+
     // ファイルポインタの宣言
     FILE *inputFile;
     FILE *outputFile;
-
+    
     // アルファベットの出現頻度を記録するための配列
     int nijigen_counts[26] = {0};   // 小文字の場合
     int sanjigen_counts[26] = {0};   // 大文字の場合
-    int ch; // 読み込んだ文字を格納
+    int ch, prev_ch = EOF; // 読み込んだ文字を格納
 
     // 入力ファイルを開く(読み込みモードで)
     inputFile = fopen("Input.txt", "r");
@@ -46,15 +54,31 @@ int main(void)
     // メインループ処理
     while ((ch = fgetc(inputFile)) != EOF)
     {
-        // 条件に応じて文字を処理
+        // 条件に応じて文字を処理(二文字)
         if (isalpha(ch))
         {
-            if (isupper(ch))
+            if (isupper(ch))                            // 大文字の時
             {
                 int index_A = ch - 'A';
                 nijigen_counts[index_A]++;
             }
-            else if (islower(ch))
+            else if (islower(ch))                      // 小文字の時
+            {
+                int index_a = ch - 'a';
+                sanjigen_counts[index_a]++;
+            }
+
+            fputc(ch, outputFile);
+        }
+        // 条件に応じて文字を処理(三文字)
+        else if (isalpha(ch))
+        {
+            if (isupper(ch))                            // 大文字の時
+            {
+                int index_A = ch - 'A';
+                nijigen_counts[index_A]++;
+            }
+            else if (islower(ch))                      // 小文字の時
             {
                 int index_a = ch - 'a';
                 sanjigen_counts[index_a]++;
