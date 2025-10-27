ac526362ed40eb93fc635eb21a055cafe1ef6933
hiro <hiroto117@Hirotovivobook.localdomain>
2025-10-27 19:39:32 +0900

    test program

--- kadai3.c
+++ kadai3.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 
 #define MAX_UNIGUE_BIGRAMS 5000
+#define MAX_UNIGUE_NRAMS 5000
+#define TOP_N 15
+
 // 2組と出現頻度を保存する構造体
 typedef struct 
 {
     char bigram[3];     // ex) 'he' + '0\' で3バイト
     int count;
 }BigramFreq;
-BigramFreq list[MAX_UNIGUE_BIGRAMS];
+BigramFreq bigram_list[MAX_UNIGUE_BIGRAMS];
 int unique_bigram_count = 0;
 
 // 3組と出現頻度を保存する構造体
@@ -17,13 +20,17 @@ typedef struct{
     char ngram[4];      // 'the' + '\0' で4バイト
     int count;
 }Ngram;
-Ngram list[MAX_UNIGUE_BIGRAMS];
+Ngram ngram_list[MAX_UNIGUE_NRAMS];
 int unique_ngram_count = 0;
 
+//---関数プロトタイプ---//
+void update_bigram_count(const char *bigram);
+void update_ngram_count(const char *ngram);
+int compare_bigram(const void *a, const void *b);
+int compare_ngram(const void *a, const void *b);
+ 
 int main(void)
 {
-   
-
 
     // ファイルポインタの宣言
     FILE *inputFile;
@@ -32,7 +39,9 @@ int main(void)
     // アルファベットの出現頻度を記録するための配列
     int nijigen_counts[26] = {0};   // 小文字の場合
     int sanjigen_counts[26] = {0};   // 大文字の場合
-    int ch, prev_ch = EOF; // 読み込んだ文字を格納
+    int prev_ch, ch = EOF; // 読み込んだ文字を格納       prev_chはEOFで初期化
+    int prevv_ch = EOF;    // 三文字用の変数
+
 
     // 入力ファイルを開く(読み込みモードで)
     inputFile = fopen("Input.txt", "r");
@@ -43,7 +52,7 @@ int main(void)
     }
 
     // 出力ファイルを開く(書き込みモードで)
-    outputFile = fopen("Output2.txt", "w");
+    outputFile = fopen("Output3.txt", "w");
     if (outputFile == NULL)
     {
         printf("エラー：出力ファイルを開けません。\n");
@@ -55,62 +64,49 @@ int main(void)
     while ((ch = fgetc(inputFile)) != EOF)
     {
         // 条件に応じて文字を処理(二文字)
-        if (isalpha(ch))
+        if (isalpha(prev_ch) && isalpha(ch))
         {
-            if (isupper(ch))                            // 大文字の時
-            {
-                int index_A = ch - 'A';
-                nijigen_counts[index_A]++;
-            }
-            else if (islower(ch))                      // 小文字の時
-            {
-                int index_a = ch - 'a';
-                sanjigen_counts[index_a]++;
-            }
-
-            fputc(ch, outputFile);
+            char current_bigram[3];
+            current_bigram[0] = tolower(prev_ch);   // 小文字で統一、prev_chに格納
+            current_bigram[1] = tolower(ch);        // chに格納         
+            current_bigram[2] = '\0';   
+            update_bigram_count(current_bigram);
         }
         // 条件に応じて文字を処理(三文字)
-        else if (isalpha(ch))
+        else if (isalpha(prevv_ch) && isalpha(prev_ch) && isalpha(ch))
         {
-            if (isupper(ch))                            // 大文字の時
-            {
-                int index_A = ch - 'A';
-                nijigen_counts[index_A]++;
-            }
-            else if (islower(ch))                      // 小文字の時
-            {
-                int index_a = ch - 'a';
-                sanjigen_counts[index_a]++;
-            }
+            char current_ngram[4];
+            current_ngram[0] = tolower(prevv_ch);
+            current_ngram[1] = tolower(prev_ch);
+            current_ngram[2] = tolower(ch);
+            current_ngram[3] = '\0';
+            update_ngram_count(current_ngram);
 
-            fputc(ch, outputFile);
         }
-        else if (ch == ' ' || ch == '\n')
-        {
-            fputc(ch, outputFile);
-        }
-        else
-        {
-            fputc(' ', outputFile);
-        }
+        prev_ch = ch;
+        prevv_ch = prevv_ch;
     }
 
     // ファイルを閉じる
     fclose(inputFile);
     fclose(outputFile);
 
-    printf("出力が完了しました。一回も出なかった文字は以下の通りです\n");
+    printf("処理中です\n");
     printf("------------------------------\n");
 
-    //大文字の場合
-    for (int i = 0; i < 26; i++)
+    //2文字の並べ替え
+    qsort(bigram_list, unique_bigram_count, sizeof(BigramFreq), compare_bigram);
+    //3文字の並べ替え
+    qsort(ngram_list, unique_ngram_count, sizeof(Ngram), compare_ngram);
+
+    //2文字の場合
+    for (int i = 0; i < unique_bigram_count; i++)
     {
         printf("%d %c\n", nijigen_counts[i], 'A' + i); // %dは出た文字の回数、%cは文字を出力する
     }
 
-    //小文字の場合
-    for (int i = 0; i < 26; i++)
+    //3文字の場合
+    for (int i = 0; i < unique_ngram_count; i++)
     {
         printf("%d %c\n", sanjigen_counts[i], 'a' + i); // %dは出た文字の回数、%cは文字を出力する
     }
